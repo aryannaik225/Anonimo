e8b0853e2e9e7600f3658126a28087fa3decc3e6
Aryan Naik <aryann2203@gmail.com>
2024-07-27 21:03:26 +0530

    1024px width hero section response done

--- anonimo/components/HeroSection.jsx
+++ anonimo/components/HeroSection.jsx
@@ -21,19 +21,19 @@ const HeroSection = () => {
         </div>
         <div className='w-full flex justify-center mt-7'>
           <a href="/FeedbackForm/Personal-Information" className='h-20 mb-20 rounded-full'>
-            <div className='btn-bg h-20 rounded-full bg-white flex justify-center items-center overflow-hidden relative mb-16' style={{width: "277px"}}>
+            <div className='btn-bg xl:h-20 xl:w-[277px] lg:h-[72px] lg:w-[256px] rounded-full bg-white flex justify-center items-center overflow-hidden relative mb-16'>
               <div className='hover-layer'></div>
               <p className='button-text'>Get Started</p>
             </div>
           </a>
         </div>
-        <div className='absolute floatee 2xl:top-[78px] 2xl:left-[139px] xl:top-[78px] xl:left-[129px] '>
+        <div className='absolute floatee 2xl:top-[78px] 2xl:left-[139px] xl:top-[78px] xl:left-[129px] lg:top-[78px] lg:left-[109px]'>
           <Image src={HandWave} alt="Hand Wave" className='pointer-events-none home-hand-wave'/>
         </div>
-        <div className='absolute floatee2 2xl:top-[102px] 2xl:right-[270.29px] xl:top-[102px] xl:right-[200.29px]'>
+        <div className='absolute floatee2 2xl:top-[102px] 2xl:right-[270.29px] xl:top-[102px] xl:right-[200.29px] lg:top-[102px] lg:right-[130.29px]'>
           <Image src={RedHeart} alt="Red Heart" className="pointer-events-none home-red-heart"/>
         </div>
-        <div className='absolute floatee3 2xl:top-[424px] 2xl:right-[101.9px] xl:top-[424px] xl:right-[1.9px]'>
+        <div className='absolute floatee3 2xl:top-[424px] 2xl:right-[101.9px] xl:top-[424px] xl:right-[1.9px] lg:top-[424px] lg:right-[1.9px]'>
           <Image src={AnonimousFace} alt="Anonymous Guy" className="pointer-events-none home-anonymous-guy"/>
         </div>
       </div>
